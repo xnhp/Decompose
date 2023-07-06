6037234aca8aa36ba149ba7b35b739c7b22338b5
Benjamin Moser <benjamin.2.moser@uni-konstanz.de>
2023-07-06 17:39:13 +0200

    utilities

---
+++ utils.py
@@ -0,0 +1,39 @@
+from enum import auto
+
+import numpy as np
+from strenum import StrEnum
+
+
+def pairwise_matrix(data, fun):
+    """
+
+    Parameters
+    ----------
+    data            1D-Array of points
+    fun     Compare two points
+
+    Returns
+    -------
+    Symmetric matrix of fun(data[i], data[j])
+    """
+    m = data.shape[0]
+    combinations_indices = np.transpose(np.triu_indices(m, 1))
+    r = np.zeros([m, m])
+    for i, j in combinations_indices:
+        d_ij = fun(data[i].squeeze(), data[j].squeeze())
+        r[i, j] = d_ij
+        r[j, i] = d_ij
+    return r
+
+
+class CustomMetric(StrEnum):
+    MEMBER_DEVIATION = auto()
+    DISTMAT_DHAT = auto()
+    DISTMAT_DISAGREEMENT = auto()
+    INDIV_ERRORS = auto()
+    EXP_MEMBER_LOSS = auto()
+    COVMAT = auto()
+
+
+class DatasetId(StrEnum):
+    MNIST = "mnist"
